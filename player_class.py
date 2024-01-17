3194177f5af70e6d317591dbb7b8e932ba945091
CapHottie <lyniguez.king@gmail.com>
2024-01-17 14:21:54 -0800

    boo

--- player_class.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, name, jersey, position, rotation):
-        self.name = name
-        self.jersey = jersey
-        self.position = position
-        self.rotation = rotation
-
